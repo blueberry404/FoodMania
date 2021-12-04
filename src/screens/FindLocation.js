460840d5a30968590ab82664b51043cb26e8a7cb
Anum Amin <developer.anum@gmail.com>
2021-12-05 01:40:59 +0500

    Platform specific handling textinput

--- src/screens/FindLocation.js
+++ src/screens/FindLocation.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View, TextInput } from 'react-native'
 import { IconButton, Button } from 'react-native-paper'
 import { SafeAreaView } from 'react-native-safe-area-context'
+
 import { COLOR_BLUE, COLOR_PINK, COLOR_TEXT_BG, COLOR_GRAY_ICON } from '../colors'
+import { isAndroid } from '../utils/platform'
 
 const FindLocation = ({ navigation }) => {
     const {
@@ -16,6 +18,8 @@ const FindLocation = ({ navigation }) => {
         contentContainer,
         buttonContainer,
     } = styles
+
+    const [addr, setAddr] = useState('')
 
     return (
         <SafeAreaView style={container} edges={['top', 'left', 'right']}>
@@ -32,15 +36,18 @@ const FindLocation = ({ navigation }) => {
                             style={textInputStyle}
                             underlineColorAndroid="transparent"
                             placeholder="Enter delivery address"
+                            keyboardType={isAndroid() ? 'visible-password' : 'ascii-capable'}
                             autoCorrect={false}
                             borderWidth={0}
+                            value={addr}
+                            onChangeText={(val) => setAddr(val)}
                         />
                         <IconButton
                             icon="close-circle-outline"
-                            style={iconView}
+                            style={[iconView, !addr && { height: 0 }]}
                             color={COLOR_GRAY_ICON}
                             size={20}
-                            onPress={() => {}}
+                            onPress={() => setAddr('')}
                         />
                         <IconButton
                             icon="crosshairs-gps"
@@ -75,13 +82,13 @@ const styles = StyleSheet.create({
     container: { flex: 1 },
     cardStyle: {
         backgroundColor: COLOR_BLUE,
-        height: 60,
+        height: isAndroid() ? 70 : 50,
         flexDirection: 'row',
         alignItems: 'center',
     },
     textContainer: {
         backgroundColor: COLOR_TEXT_BG,
-        height: 40,
+        height: '80%',
         width: '80%',
         borderRadius: 10,
         borderWidth: 1,
